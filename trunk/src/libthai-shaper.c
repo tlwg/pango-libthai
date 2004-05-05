ad08d41475844a23d48c95e98f2ace238961394b
thep <thep@5124d66a-a8c1-4197-beb6-13d3f394dfd5>
2004-05-05 23:04:14 +0000

    Many changes according to new module specification introduced in pango 1.3/1.4. Note that this requires unreleased pango until GNOME Bug #141931 is fixed.

--- trunk/src/libthai-shaper.c
+++ trunk/src/libthai-shaper.c
@@ -17,7 +17,7 @@
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.         See the GNU
  * Library General Public License for more details.
  *
  * You should have received a copy of the GNU Library General Public
@@ -37,10 +37,10 @@
 
 static void
 add_glyph (ThaiFontInfo     *font_info, 
-	   PangoGlyphString *glyphs, 
-	   gint              cluster_start, 
-	   PangoGlyph        glyph,
-	   gboolean          combining)
+           PangoGlyphString *glyphs, 
+           gint              cluster_start, 
+           PangoGlyph        glyph,
+           gboolean          combining)
 {
   PangoRectangle ink_rect, logical_rect;
   gint index = glyphs->num_glyphs;
@@ -53,28 +53,17 @@ add_glyph (ThaiFontInfo     *font_info,
   glyphs->log_clusters[index] = cluster_start;
 
   pango_font_get_glyph_extents (font_info->font,
-				glyphs->glyphs[index].glyph, &ink_rect, &logical_rect);
+                                glyphs->glyphs[index].glyph, &ink_rect, &logical_rect);
 
-  if (combining)
+  if (combining || logical_rect.width > 0)
     {
-      glyphs->glyphs[index].geometry.x_offset =
-	glyphs->glyphs[index - 1].geometry.width;
-      glyphs->glyphs[index].geometry.width =
-	logical_rect.width + glyphs->glyphs[index - 1].geometry.width;
-      glyphs->glyphs[index - 1].geometry.width = 0;
+      glyphs->glyphs[index].geometry.x_offset = 0;
+      glyphs->glyphs[index].geometry.width = logical_rect.width;
     }
   else
     {
-      if (logical_rect.width > 0)
-        {
-	 glyphs->glyphs[index].geometry.x_offset = 0;
-	 glyphs->glyphs[index].geometry.width = logical_rect.width;
-	}
-      else
-        {
-	 glyphs->glyphs[index].geometry.x_offset = ink_rect.width;
-	 glyphs->glyphs[index].geometry.width = ink_rect.width;
-        }
+      glyphs->glyphs[index].geometry.x_offset = ink_rect.width;
+      glyphs->glyphs[index].geometry.width = ink_rect.width;
     }
   glyphs->glyphs[index].geometry.y_offset = 0;
 }
@@ -90,42 +79,12 @@ make_font_glyphs (ThaiFontInfo *font_info,
     {
       pango_glyphs[i]
         = (font_info->font_set == THAI_FONT_NONE) ?
-            (*font_info->make_unknown_glyph) (font_info, log_glyphs[i]) :
-            (*font_info->make_glyph) (font_info, log_glyphs[i]);
+            libthai_make_unknown_glyph (font_info, log_glyphs[i]) :
+            libthai_make_glyph (font_info, log_glyphs[i]);
     }
 
   return i;
 }
-
-/* Map from code point to group used for rendering with XTIS fonts
- * (0 == base character)
- */
-static const char xtis_groups[32] = {
-  0, 1, 0, 0, 1, 1, 1, 1,
-  1, 1, 1, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 2,
-  2, 2, 2, 2, 2, 2, 1, 0
-};
-
-/* Map from code point to index within group 1
- * (0 == no combining mark from group 1)
- */   
-static const char xtis_group1_map[32] = {
-  0, 1, 0, 0, 2, 3, 4, 5,
-  6, 7, 8, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0
-};
-
-/* Map from code point to index within group 2
- * (0 == no combining mark from group 2)
- */   
-static const char xtis_group2_map[32] = {
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 1,
-  2, 3, 4, 5, 6, 7, 1, 0
-};
 
 static int
 make_logical_glyphs (ThaiFontInfo *font_info, struct thcell_t tis_cell,
@@ -146,53 +105,20 @@ make_logical_glyphs (ThaiFontInfo *font_info, struct thcell_t tis_cell,
       case THAI_FONT_TIS_WIN:
         return th_render_cell_win (tis_cell, log_glyphs, n_log_glyphs, TRUE);
   
-      case THAI_FONT_XTIS:
-        /* If we are rendering with an XTIS font, we try to find a precomposed
-         * glyph for the cluster.
-         */
-	if (tis_cell.base < 0x80)
-          {
-            log_glyphs[0] = tis_cell.base;
-            return 1;
-          }
-	else
-          {
-            int        xtis_index;
-            int        i;
-            PangoGlyph glyph;
-
-            xtis_index = 0x100 * (tis_cell.base - 0x80) + 0x30;
-            if (tis_cell.hilo)
-              xtis_index += 8 * xtis_group1_map[tis_cell.hilo - 0xd0];
-            if (tis_cell.top)
-              xtis_index += xtis_group2_map[tis_cell.top - 0xd0];
-            glyph = (*font_info->make_glyph) (font_info, xtis_index);
-            if (pango_x_has_glyph (font_info->font, glyph))
-              {
-                log_glyphs[0] = xtis_index;
-                return 1;
-              }
-            i = 0;
-            log_glyphs[i++] = 0x100 * (tis_cell.base - 0x80) + 0x30;
-            if (tis_cell.hilo)
-              log_glyphs[i++] = 0x100 * (tis_cell.hilo - 0x80) + 0x30;
-            if (tis_cell.top)
-              log_glyphs[i++] = 0x100 * (tis_cell.top - 0x80) + 0x30;
-            return i;
-          }
-  
       default:
         return 0;
     }
 }
 
 void 
-thai_engine_shape (ThaiFontInfo     *font_info,
-		   const char       *text,
-		   gint              length,
-		   PangoAnalysis    *analysis,
-		   PangoGlyphString *glyphs)
+libthai_engine_shape (PangoEngineShape *engine,
+                      PangoFont        *font,
+                      const char       *text,
+                      gint              length,
+                      PangoAnalysis    *analysis,
+                      PangoGlyphString *glyphs)
 {
+  ThaiFontInfo *font_info;
   thchar_t *tis_text;
   int      tis_length;
   int      i, j, n;
@@ -202,6 +128,8 @@ thai_engine_shape (ThaiFontInfo     *font_info,
   tis_length = strlen (tis_text);
 
   pango_glyph_string_set_size (glyphs, 0);
+
+  font_info = libthai_get_font_info (font);
 
   for (i = 0; i < tis_length; /* nop */)
     {
