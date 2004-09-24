d121952cc4491fd8e4f71533287ec276e9ddf1b9
nobody <nobody@5124d66a-a8c1-4197-beb6-13d3f394dfd5>
2004-09-24 08:01:34 +0000

    This commit was manufactured by cvs2svn to create tag 'r_0_1_3'.

---
+++ tags/r_0_1_3/src/libthai-ot.h
@@ -0,0 +1,18 @@
+/* Pango
+ * libthai-ot.h:
+ *
+ * Copyright (C) 2004 Theppitak Karoonboonyanan
+ * Author: Theppitak Karoonboonyanan <thep@linux.thai.net>
+ */
+
+#ifndef __LIBTHAI_OT_H__
+#define __LIBTHAI_OT_H__
+
+#include <pango/pango-ot.h>
+
+void 
+libthai_ot_shape (PangoFont        *font,
+                  PangoGlyphString *glyphs);
+
+#endif /* __LIBTHAI_OT_H__ */
+
