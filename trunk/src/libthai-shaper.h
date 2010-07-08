b4a7e582d91fc6266aa4916e793ca7248a7e03bf
thep-svn <thep-svn@5124d66a-a8c1-4197-beb6-13d3f394dfd5>
2010-07-08 07:21:17 +0000

    * src/*.{c,h}: Update copyright notice and FSF address.

--- trunk/src/libthai-shaper.h
+++ trunk/src/libthai-shaper.h
@@ -1,3 +1,35 @@
+/* Pango
+ * libthai-shaper.h:
+ *
+ * Copyright (C) 1999 Red Hat Software
+ * Author: Owen Taylor <otaylor@redhat.com>
+ *
+ * Software and Language Engineering Laboratory, NECTEC
+ * Author: Theppitak Karoonboonyanan <thep@links.nectec.or.th>
+ *
+ * Copyright (c) 1996-2000 by Sun Microsystems, Inc.
+ * Author: Chookij Vanatham <Chookij.Vanatham@Eng.Sun.COM>
+ *
+ * Copyright (C) 2003-2010 Theppitak Karoonboonyanan
+ * Author: Theppitak Karoonboonyanan <thep@linux.thai.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ */
+
+
 #ifndef __THAI_SHAPER_H__
 #define __THAI_SHAPER_H__
 
